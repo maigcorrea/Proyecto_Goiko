393eaf4a47cd6e84d521d9e6b455d429dc518edb
unknown <anamaitegarciacorrea@gmail.com>
2024-10-28 13:46:56 +0100

    Arreglo algunas cosas y empiezo el menu

--- style.js
+++ style.js
@@ -1,8 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
 let btnAbrir=document.querySelector(".iconos svg:nth-child(2)");//El icono para abrir el menú
 let desp=document.querySelector(".desplegable");
 
-btnAbrir.addEventListener("click",()=>{ //Cuando hago click en el icono se despliega el menu       
-    desp.style.top="100%";
+btnAbrir.addEventListener("click",()=>{ //Cuando hago click en el icono se despliega el menu   
+    console.log("CLICK DETECTADO");
+    desp.style.top="0%";
     desp.style.transition="top 0.5s linear";
-    desp.style.width="100%";
+    desp.style.height="100vh";
+    desp.style.backgroundColor="#ee2737";
+    desp.style.overflow = "hidden"
+    // desp.style.display="flex";
+    // desp.style.flexDirection="column";
+    // desp.style.alignItems="center";
+})
 })
